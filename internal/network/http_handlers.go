7e8522dff758ec205aa99682d46a3a82ce3eaaf1
Bond <Silverstonejr@gmail.com>
2026-09-07 22:21:39 +0300

    feat: created a http server as the first impleamentation for Raven
    BRIEF:
    - Learnt how to conn go to a DB
    - Restructured project structure for simplicity and simpler nav to avoid
      overwhelming sensation during developement
    - chore: update .gitignore
    TODO:
    - Upgrade this HTTP server to a websocket + connect a DB to it
    - Research into the frontned bit and design the screens or so
    - Research on what parts of the project need refractoring using other langs

--- internal/network/http_handlers.go
+++ internal/network/http_handlers.go
@@ -12,11 +12,18 @@ type User struct {
 	Name string `json:"name"`
 }
 
+// this acts as the in-memory "db"
 var userCache = make(map[int]User)
 
+// this enables many goroutines to READ data at the same time
+// but only allows ONE goroutine to write data at a time
+// this is only responsbile for the Lock() and so on "stuff"
 var cacheMutex sync.RWMutex
 
+var lastID int // for the in-memory "db" auto-inc bug incase of deletion
+
 func main() {
+	// creates the router called mux (Multiplexer)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
 
@@ -33,20 +40,25 @@ func handleRoot(w http.ResponseWriter, r *http.Request) {
 }
 
 func deleteUser(w http.ResponseWriter, r *http.Request) {
+	// the converstion of the users req + handle the errors
 	id, err := strconv.Atoi(r.PathValue("id"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
+	// if above is a success, look for the user in the map using the id provided
+	// plus do the error checking
 	if _, ok := userCache[id]; !ok {
 		http.Error(w, "User not found", http.StatusBadRequest)
 		return
 	}
+	// if the above is a success, write to the in-mem "db"
+	// Lock() gives the responsible go routine write access and blocks other readers and writers
 	cacheMutex.Lock()
-	defer cacheMutex.Unlock()
-	delete(userCache, id)
+	defer cacheMutex.Unlock() // once done, unlock read and write access
+	delete(userCache, id)     // delete the user
 
-	w.WriteHeader(http.StatusOK)
+	w.WriteHeader(http.StatusOK) // tell the clients browser everything went ok
 }
 
 func getUser(w http.ResponseWriter, r *http.Request) {
@@ -55,30 +67,34 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	// lock the READING PART ONLY
+	// lock the WRITING PART ONLY, NO ONE can write to it (hence the R for read in the name)
 	cacheMutex.RLock()
-	defer cacheMutex.RUnlock()
+	defer cacheMutex.RUnlock() // unlock when done
 	user, ok := userCache[id]
 	if !ok {
 		http.Error(w, "User not found", http.StatusNotFound)
 		return
 	}
+	// if the above is successful, give back to the client
+	// data in form of JSON as the header
+	// NOTE: this doesn't send any bytes over the net yet...
 	w.Header().Set("Content-Type", "application/json")
-	// parse data to JSON (marshaling)
-	j, err := json.Marshal(user)
+	// parse(encode) data to JSON (marshaling)
+	data, err := json.Marshal(user)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(j)
+	// quick note: u only need to call w.WriteHeader only when u want to return a status code without..
+	// an err body.e.g w.WriteHeader(http.StatusNoContent)
+	w.Write(data) // this does the returning the status code + sends over the data via the internet
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
 	var user User
-	// takes in the body we got from the requst and decodes it to JSON and "appends" it...
-	// to our struct
+	// takes in the body we got from the requst and decodes it to JSON and decodes it...
+	// to the struct
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
@@ -88,8 +104,13 @@ func createUser(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Name is required", http.StatusBadRequest)
 		return
 	}
+	// lock mutation
 	cacheMutex.Lock()
-	defer cacheMutex.Unlock()
-	userCache[len(userCache)+1] = user
-	w.WriteHeader(http.StatusCreated)
+	lastID++                           // increament by 1
+	defer cacheMutex.Unlock()          // unlock when below is done
+	userCache[len(userCache)+1] = user // do the increamentation to the user
+	w.WriteHeader(http.StatusCreated)  // send a created successfully status code
+
+	// encode the response struct/data in JSON format and send it back to the client
+	json.NewEncoder(w).Encode(user)
 }
